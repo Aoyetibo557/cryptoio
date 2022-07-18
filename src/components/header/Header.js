fd7804507b4d4f9ef52583fc530a2f07acf4bde2
aoyetibo557 <aoyetibo@gmail.com>
2022-07-18 16:01:17 -0400

    Just finished making Login page, Register page and header responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,11 +3,18 @@ import "./Header.css";
 import HeaderLogo from "../../assets/nobgLogo.png";
 import { Link } from "react-router-dom";
 import Button from '../button/Button';
+import {TbMenu } from "react-icons/tb";
+import { MdClose } from "react-icons/md";
+
+
+
 
 
 function Header() {
   //get the url of the current page
   const [url, setUrl] = useState(window.location.pathname);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const urlPath = url.split("/")[2] || "home";
 
   useEffect(() => {
@@ -21,34 +28,29 @@ function Header() {
         <Link to='/'>
             <img src={HeaderLogo} alt="logo" className='header__logo'/>
         </Link>
-        {/* {urlPath === "home" ? ( */}
-          <nav className='header__nav-home'>
-            <div className='header__nav-div'>
-              <Link to='/' className='header__nav-link'>Home</Link>
-              <Link to='/mobile-app' className='header__nav-link'>Mobile app</Link>
-              <Link to='/markets' className='header__nav-link'>Markets</Link>
-              {/* <Link to='/' className='header__nav-link'>Blogs</Link> */}
-            </div>
 
-            <div className='header__nav-div-right'>
-              <Link to='/onboarding/login' className='header__nav-link'>Login</Link>
-              <Button 
-                url='/onboarding/login'
-                label='Get Started'
-                color='primary'
-              />
-            </div>
-          </nav>
-        {/* ):( */}
-          {/* <nav className='header__nav'>
-              {urlPath === "register" ? (
-                <Link to='/onboarding/login' className='header__nav-link'>Login</Link>
-              ):(
-                <Link to='/onboarding/register' className='header__nav-link'>Register</Link>
-              )}
+        <div className='hamburger'>
+          {menuOpen ? <MdClose className='header__icon' onClick={() => setMenuOpen(false)}/> : <TbMenu className='header__icon' onClick={() => setMenuOpen(true)}/>}
+        </div>
 
-          </nav> */}
-        {/* )} */}
+        <nav className= { !menuOpen ? "header__nav-home" : "header__nav-home active"}>
+          <div className='header__nav-div'>
+            <Link to='/' className='header__nav-link'>Home</Link>
+            <Link to='/mobile-app' className='header__nav-link'>Mobile app</Link>
+            <Link to='/markets' className='header__nav-link'>Markets</Link>
+            {/* <Link to='/' className='header__nav-link'>Blogs</Link> */}
+          </div>
+
+          <div className='header__nav-div-right'>
+            <Link to='/onboarding/login' className='header__nav-link'>Login</Link>
+            <Button 
+              url='/onboarding/login'
+              label='Get Started'
+              color='primary'
+            />
+          </div>
+        </nav>
+
     </header>
   )
 }
